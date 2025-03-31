50cb2f0b3afff180fb98b4ba9c5c847ca8749332
lasse <77295879+lassejlv@users.noreply.github.com>
2025-03-31 10:58:28 +0200

    feat: streamline user routes and improve error handling in main application file

--- src/main.ts
+++ src/main.ts
@@ -1,55 +1,54 @@
-import { Hono } from "hono";
-import { zValidator } from "@hono/zod-validator";
-import { db } from "./db/index.ts";
-import { user } from "./db/schema.ts";
-import { z } from "zod";
-import { eq } from "drizzle-orm";
-import bjs from "bcryptjs";
+import { Hono } from 'hono'
+import { zValidator } from '@hono/zod-validator'
+import { db } from './db/index.ts'
+import { user } from './db/schema.ts'
+import { z } from 'zod'
+import { eq } from 'drizzle-orm'
+import bjs from 'bcryptjs'
 
 const CreateUserSchema = z.object({
   username: z.string().min(3).max(200),
   email: z.string().email(),
   password: z.string().min(8).max(64),
-});
+})
 
-const app = new Hono();
+const app = new Hono()
 
-app.get("/", (c) => c.text("Hello Hono!"));
+app.get('/', (c) => c.text('Hello Hono!'))
 
-app.get("/users", async (c) => {
-  const users = await db.query.user.findMany();
-  return c.json(users);
-});
+app.get('/users', async (c) => {
+  const users = await db.query.user.findMany()
+  return c.json(users)
+})
 
-app.post("/users", zValidator("json", CreateUserSchema), async (c) => {
-  const data = c.req.valid("json");
+app.post('/users', zValidator('json', CreateUserSchema), async (c) => {
+  const data = c.req.valid('json')
 
-  const password_hash = await bjs.hash(data.password, 10);
+  const password_hash = await bjs.hash(data.password, 10)
 
-  const new_user = await db.insert(user).values({
-    username: data.username,
-    email: data.email,
-    password_hash,
-  }).returning().get();
-  return c.json(new_user);
-});
+  const new_user = await db
+    .insert(user)
+    .values({
+      username: data.username,
+      email: data.email,
+      password_hash,
+    })
+    .returning()
+  return c.json(new_user[0])
+})
 
-app.get(
-  "/users/:id",
-  zValidator("param", z.object({ id: z.string().uuid() })),
-  async (c) => {
-    const id = c.req.valid("param").id;
+app.get('/users/:id', zValidator('param', z.object({ id: z.string().uuid() })), async (c) => {
+  const id = c.req.valid('param').id
 
-    const data = await db.query.user.findFirst({ where: eq(user.id, id) });
-    if (!data) throw new Error("User not found");
+  const data = await db.query.user.findFirst({ where: eq(user.id, id) })
+  if (!data) throw new Error('User not found')
 
-    return c.json(data);
-  },
-);
+  return c.json(data)
+})
 
 app.onError((err, c) => {
-  console.log(err);
-  return c.json({ error: err.message }, 500);
-});
+  console.log(err)
+  return c.json({ error: err.message }, 500)
+})
 
-Deno.serve(app.fetch);
+Deno.serve(app.fetch)
