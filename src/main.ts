e7a2c7ab4852e464ae3b0757b83987855b0397c5
lasse <77295879+lassejlv@users.noreply.github.com>
2025-03-31 10:27:11 +0200

    feat: add endpoint to retrieve user by ID with validation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { zValidator } from '@hono/zod-validator'
 import { db } from './db/index.ts'
 import { user } from './db/schema.ts'
 import { z } from 'zod'
+import { eq } from 'drizzle-orm'
 
 const app = new Hono()
 
@@ -17,6 +18,15 @@ app.post('/users', zValidator('json', z.object({ username: z.string().min(3).max
   const data = c.req.valid('json')
   const new_user = await db.insert(user).values({ username: data.username }).returning().get()
   return c.json(new_user)
+})
+
+app.get('/users/:id', zValidator('param', z.object({ id: z.string().uuid() })), async (c) => {
+  const id = c.req.valid('param').id
+
+  const data = await db.query.user.findFirst({ where: eq(user.id, id) })
+  if (!data) throw new Error('User not found')
+
+  return c.json(data)
 })
 
 app.onError((err, c) => {
